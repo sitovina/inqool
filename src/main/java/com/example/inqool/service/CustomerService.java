b1f8135d820bd728ad57737f29acefd655605a35
yeet <t.sith96@gmail.com>
2025-06-16 22:22:21 +0200

    customer service

---
+++ src/main/java/com/example/inqool/service/CustomerService.java
@@ -0,0 +1,41 @@
+package com.example.inqool.service;
+
+import com.example.inqool.model.Customer;
+import com.example.inqool.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> getAllCourts() {
+        return customerRepository.findAll();
+    }
+
+    public Optional<Customer> getCourtById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    public Customer createCustomer(Customer court) {
+        return customerRepository.save(court);
+    }
+
+    public void deleteCourt(Long id) {
+        if (customerRepository.existsById(id)) {
+            customerRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Court with id: " + id + " not found");
+        }
+    }
+
+}

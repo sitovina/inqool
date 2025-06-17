90ce9de7a32a1a54996d9bc2083d786a77399aa9
yeet <t.sith96@gmail.com>
2025-06-17 10:31:50 +0200

    add start and end dates to test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,6 +4,7 @@ INSERT INTO Customer (id, first_name, last_name, phone) VALUES (2, 'bara', 'crad
 INSERT INTO Court (id, cost) VALUES (1, 45.0);
 INSERT INTO Court (id, cost) VALUES (2, 23.0);
 
-INSERT INTO Reservation (id, court_id, owner_id, cost) VALUES (5, 1, 1, 12);
-INSERT INTO Reservation (id, court_id, owner_id, cost) VALUES (6, 1, 2, 12);
-INSERT INTO Reservation (id, court_id, owner_id, cost) VALUES (7, 2, 2, 12);
+INSERT INTO Reservation (id, court_id, owner_id, cost, reservation_start, reservation_end) VALUES (5, 1, 1, 12, '2020-08-21 00:00:00', '2020-08-21 01:00:00');
+INSERT INTO Reservation (id, court_id, owner_id, cost, reservation_start, reservation_end) VALUES (6, 1, 2, 12, '2025-06-17 15:00:00', '2025-06-17 16:00:00');
+INSERT INTO Reservation (id, court_id, owner_id, cost, reservation_start, reservation_end) VALUES (7, 1, 2, 12, '2025-06-17 08:00:00', '2025-06-17 09:00:00');
+INSERT INTO Reservation (id, court_id, owner_id, cost) VALUES (8, 2, 2, 12);
